a7e378f013d226bc91d595636f280d6ab0b151a7
ipayne18 <student@ustech-04.local>
2018-05-22 09:45:44 -1000

    added requests to requirements.txt

--- ItemGuide.py
+++ ItemGuide.py
@@ -162,4 +162,4 @@ def classDisplay(playerName, playerClass):
         return render_template("guildResults.html")
 if __name__ == "__main__":
     app.debug = False
-    app.run(host="0.0.0.0", port="8080")
+    app.run(host="0.0.0.0", port=8080)
