34ccff5490e401ac29cb2a2ffbeaeaf3a89b9f00
ipayne18 <student@ustech-04.local>
2018-05-22 15:03:32 -1000

    changed port

--- ItemGuide.py
+++ ItemGuide.py
@@ -162,4 +162,5 @@ def classDisplay(playerName, playerClass):
         return render_template("guildResults.html")
 if __name__ == "__main__":
     app.debug = False
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
